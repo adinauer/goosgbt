a1d3acc7c1f1036d1478c62b42c786abe9c79232
Alexander Dinauer <alexander@dinauer.at>
2013-04-29 17:57:05 +0200

    unignore MainWindowTest because it works when running tests with gradle

--- auction-sniper/src/test/java/at/dinauer/goosgbt/MainWindowTest.java
+++ auction-sniper/src/test/java/at/dinauer/goosgbt/MainWindowTest.java
@@ -3,7 +3,6 @@ package at.dinauer.goosgbt;
 
 import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.objogate.wl.swing.probe.ValueMatcherProbe;
@@ -14,7 +13,6 @@ public class MainWindowTest {
     private final MainWindow          mainWindow = new MainWindow(tableModel);
     private final AuctionSniperDriver driver     = new AuctionSniperDriver(100);
     
-    @Ignore("does not close the window which causes other tests to break")
     @Test
     public void makeUsersRequestWhenJoinButtonClicked() {
         final ValueMatcherProbe<String> buttonProbe = new ValueMatcherProbe<>(equalTo("an item-id"), "join request");
